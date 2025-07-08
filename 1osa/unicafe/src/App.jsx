b0cd817e73ba297a5e65f1e5f3cf43645b16d02a
Urho <urho.lohikoski@gmail.com>
2025-07-08 14:31:52 +0300

    unicafe step 2 done

--- 1osa/unicafe/src/App.jsx
+++ 1osa/unicafe/src/App.jsx
@@ -17,7 +17,7 @@ const App = () => {
   const handleNeutralClick = () => {
     setNCount(neutralCount + 1)
   }
-  
+
   return (
     <div>
       <h1>give feedback</h1>
@@ -25,9 +25,12 @@ const App = () => {
       <button onClick={handleNeutralClick}>neutral</button>
       <button onClick={handleBadClick}>bad</button>
       <h2>statistics</h2>
-      <Stat name='good' count={goodCount} ></Stat>
-      <Stat name='neutral' count={neutralCount}></Stat>
-      <Stat name='bad' count={badCount}></Stat>
+      <Stat name='good:' count={goodCount} ></Stat>
+      <Stat name='neutral:' count={neutralCount}></Stat>
+      <Stat name='bad:' count={badCount}></Stat>
+      <SumStat goodCount={goodCount} neutralCount={neutralCount} badCount={badCount}></SumStat>
+      <AverageStat goodCount={goodCount} neutralCount={neutralCount} badCount={badCount}></AverageStat>
+      <PositivesStat goodCount={goodCount} neutralCount={neutralCount} badCount={badCount}></PositivesStat>
     </div>
   )
 }
@@ -39,6 +42,39 @@ const Stat = ({name, count}) => {
     </div>
   )
 }
+
+const SumStat = ({goodCount, neutralCount, badCount}) => {
+  const sum = goodCount + neutralCount + badCount
+  return (
+    <div>
+      sum of all: {sum}
+    </div>
+  )
+}
+
+const AverageStat = ({goodCount, neutralCount, badCount}) => {
+  const average = (goodCount + neutralCount + badCount)/3
+  return (
+    <div>
+      average of scores: {average}
+    </div>
+  )
+}
+
+const PositivesStat = ({goodCount, neutralCount, badCount}) => {
+  const percentage = goodCount/(goodCount + neutralCount + badCount)
+  return (
+    <div>
+      percentage of positives: {percentage}
+    </div>
+  )
+}
+
+const Sum = ({int1, int2, int3}) => {int1+int2+int3}
+
+const Average = ({int1, int2, int3}) => {(int1+int2+int3)/3}
+
+const Positive = ({pos, neut, neg}) => {pos/(pos+neut+neg)}
 
 
 export default App
