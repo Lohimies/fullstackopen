ce6f58e6ddc545d25b3842d462c5182732ff2ec2
Urho <urho.lohikoski@gmail.com>
2025-07-08 15:27:19 +0300

    unicafe step 5(1.10) done. Button and StatisticLine refactored as their own components. Commit before this was for step 4(1.9)

--- 1osa/unicafe/src/App.jsx
+++ 1osa/unicafe/src/App.jsx
@@ -21,42 +21,26 @@ const App = () => {
   return (
     <div>
       <h1>give feedback</h1>
-      <button onClick={handleGoodClick}>good</button>
-      <button onClick={handleNeutralClick}>neutral</button>
-      <button onClick={handleBadClick}>bad</button>
+      <Button text={'good'} onClick={handleGoodClick}></Button>
+      <Button text={'neutral'} onClick={handleNeutralClick}></Button>
+      <Button text={'bad'} onClick={handleBadClick}></Button>
       <Statistics goodCount={goodCount} neutralCount={neutralCount} badCount={badCount}></Statistics>
     </div>
   )
 }
 
+const Button = ({text, onClick}) => {
+  return(
+    <button onClick={onClick}>{text}</button>
+  )
+}
+
 const Statistics = ({goodCount, neutralCount, badCount}) => {
 
-  const SumStat = ({goodCount, neutralCount, badCount}) => {
     const sum = goodCount + neutralCount + badCount
-    return (
-      <div>
-        sum of all: {sum}
-      </div>
-    )
-  }
-
-  const AverageStat = ({goodCount, neutralCount, badCount}) => {
     const average = (goodCount - badCount)/(goodCount + neutralCount + badCount)
-    return (
-      <div>
-        average of scores: {average}
-      </div>
-    )
-  }
-
-  const PositivesStat = ({goodCount, neutralCount, badCount}) => {
     const percentage = goodCount/(goodCount + neutralCount + badCount)
-    return (
-      <div>
-        percentage of positives: {percentage}
-      </div>
-    )
-  }
+
   if ((goodCount+neutralCount+badCount) === 0) {
     return (
       <div>
@@ -68,17 +52,21 @@ const Statistics = ({goodCount, neutralCount, badCount}) => {
   return(
     <div>
       <h2>Statistics</h2>
-      <div>good: {goodCount}</div>
-      <div>neutral: {neutralCount}</div>
-      <div>bad: {badCount}</div>
-      <SumStat goodCount={goodCount} neutralCount={neutralCount} badCount={badCount}></SumStat>
-      <AverageStat goodCount={goodCount} neutralCount={neutralCount} badCount={badCount}></AverageStat>
-      <PositivesStat goodCount={goodCount} neutralCount={neutralCount} badCount={badCount}></PositivesStat>
+      <StatisticLine text={'Good reviews:'} value={goodCount}></StatisticLine>
+      <StatisticLine text={'Neutral reviews:'} value={neutralCount}></StatisticLine>
+      <StatisticLine text={'Bad reviews:'} value={badCount}></StatisticLine>
+      <StatisticLine text={'Sum of all reviews:'} value={sum}></StatisticLine>
+      <StatisticLine text={'Average of reviews:'} value={average}></StatisticLine>
+      <StatisticLine text={'Percentage of positive reviews:'} value={percentage}></StatisticLine>
     </div>
   )
 }
 
-
+const StatisticLine = ({text, value}) => {
+  return(
+    <div>{text} {value}</div>
+  )
+}
 
 
 export default App
