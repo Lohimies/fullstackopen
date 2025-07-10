0f79d482ca4c064e1470b6eb7241744ed0867b7a
Urho <urho.lohikoski@gmail.com>
2025-07-10 11:20:26 +0300

    Kurssitiedot step9(2.4) done. Works with arbitrary number of courses. Total text is changed to bold.

--- 2osa/kurssitiedot2osa/src/App.jsx
+++ 2osa/kurssitiedot2osa/src/App.jsx
@@ -1,29 +1,52 @@
 const App = () => {
-  const course = {
-    name: 'Half Stack application development',
-    id: 1,
-    parts: [
-      {
-        name: 'Fundamentals of React',
-        exercises: 10,
-        id: 1
-      },
-      {
-        name: 'Using props to pass data',
-        exercises: 7,
-        id: 2
-      },
-      {
-        name: 'State of a component',
-        exercises: 14,
-        id: 3
-      }
-    ]
-  }
+  const courses = [
+    {
+      name: 'Half Stack application development',
+      id: 1,
+      parts: [
+        {
+          name: 'Fundamentals of React',
+          exercises: 10,
+          id: 1
+        },
+        {
+          name: 'Using props to pass data',
+          exercises: 7,
+          id: 2
+        },
+        {
+          name: 'State of a component',
+          exercises: 14,
+          id: 3
+        },
+        {
+          name: 'Redux',
+          exercises: 11,
+          id: 4
+        }
+      ]
+    }, 
+    {
+      name: 'Node.js',
+      id: 2,
+      parts: [
+        {
+          name: 'Routing',
+          exercises: 3,
+          id: 1
+        },
+        {
+          name: 'Middlewares',
+          exercises: 7,
+          id: 2
+        }
+      ]
+    }
+  ]
 
   return (
     <div>
-      <Course course={course} />
+      {courses.map((course) => <Course course={course} key={course.id}></Course>)}
     </div>
   )
 }
@@ -67,7 +90,7 @@ const Total = ({course}) => {
   const exercisesArray = course.parts.map((part) => part.exercises)
   console.log('Total', exercisesArray)
   return (
-    <p>Total number of exercises: {exercisesArray.reduce((sum, currentVal) => sum + currentVal)}</p>
+    <b>Total number of exercises: {exercisesArray.reduce((sum, currentVal) => sum + currentVal)}</b>
   )
 }
 
