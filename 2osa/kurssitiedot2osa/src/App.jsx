343f6f06fd7e01ad7f1c1abd4fac20585b2e7ea5
Urho <urho.lohikoski@gmail.com>
2025-07-10 10:57:14 +0300

    kurssitiedot step 6(2.1) done. Course component implemented with Content mapping the parts of the course to render

---
+++ 2osa/kurssitiedot2osa/src/App.jsx
@@ -0,0 +1,72 @@
+const App = () => {
+  const course = {
+    name: 'Half Stack application development',
+    id: 1,
+    parts: [
+      {
+        name: 'Fundamentals of React',
+        exercises: 10,
+        id: 1
+      },
+      {
+        name: 'Using props to pass data',
+        exercises: 7,
+        id: 2
+      },
+      {
+        name: 'State of a component',
+        exercises: 14,
+        id: 3
+      }
+    ]
+  }
+
+  return (
+    <div>
+      <Course course={course} />
+    </div>
+  )
+}
+
+const Course = ({course}) => {
+  return (
+    <div>
+      <Header courseName={course.name}></Header>
+      <Content course={course}></Content>
+    </div>
+  )
+}
+
+const Header = ({courseName}) => {
+  console.log('header', courseName)
+  return (
+    <h1>{courseName}</h1>
+  )
+}
+
+const Content = ({course}) => {
+  console.log('Content', course)
+  return (
+    <div>
+      {course.parts.map((part) => <p key={part.id}>{part.name} {part.exercises}</p>)}
+    </div>
+  )
+}
+
+const Part = (props) => {
+  console.log('Part', props)
+  return (
+    <p>
+      {props.part.name} {props.part.exercises}
+    </p>
+  )
+}
+
+const Total = ({course}) => {
+  console.log('Total', course)
+  return (
+    <p>Number of exercises {course.parts[0].exercises + course.parts[1].exercises + course.parts[2].exercises}</p>
+  )
+}
+
+export default App
